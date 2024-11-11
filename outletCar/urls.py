1f0c08898ab5d1110742b902c6725f4eeba46efa
aitormolero <aitor.molero@opendeusto.es>
2024-11-11 12:31:51 +0100

    coches por categoria

--- outletCar/urls.py
+++ outletCar/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('appOutletCar/', include('appOutletCar.urls')),
+    path('', include('appOutletCar.urls')),
     path('admin/', admin.site.urls),
 ]
