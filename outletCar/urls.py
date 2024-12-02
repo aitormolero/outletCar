ce4f86a177e87a9ecab037d47e51e3cb312c153c
Endika <93043833+eblancocabana@users.noreply.github.com>
2024-12-03 00:49:12 +0100

    Update urls.py

--- outletCar/urls.py
+++ outletCar/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns = [
     path('', include('appOutletCar.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
