efae7c7530c606eb28a59447ba5d26f6de6ae65d
Endika <93043833+eblancocabana@users.noreply.github.com>
2024-12-03 00:18:40 +0100

    Update settings.py

--- outletCar/settings.py
+++ outletCar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9r%ob%om*zl@es(a%1+&b%98+%mh^@0z!6rxagc6s5(ay^a2ga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['outletcar.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
