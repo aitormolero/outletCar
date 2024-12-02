d97aaf3fa199dd379980ecdee548b8d95d6f9e25
Endika <93043833+eblancocabana@users.noreply.github.com>
2024-12-03 00:47:37 +0100

    Update urls.py

--- outletCar/urls.py
+++ outletCar/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
